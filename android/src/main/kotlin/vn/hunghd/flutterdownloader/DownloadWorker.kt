af8da45a9b0a1422a297db0762045302f080a51f
Masum Billah Sanjid <54928529+sanjidbillah@users.noreply.github.com>
2023-09-27 21:10:52 +0600

    Update DownloadWorker.kt

--- android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
+++ android/src/main/kotlin/vn/hunghd/flutterdownloader/DownloadWorker.kt
@@ -414,7 +414,7 @@ class DownloadWorker(context: Context, params: WorkerParameters) :
                         taskDao!!.updateTask(id.toString(), DownloadStatus.RUNNING, progress)
                         updateNotification(
                             context,
-                            actualFilename,
+                            "Downloading",
                             DownloadStatus.RUNNING,
                             progress,
                             null,
